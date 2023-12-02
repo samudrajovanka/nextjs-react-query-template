1083e9b25b926dc4e5e989feaba796c70777fabf
unknown <samudrajovanka20@gmail.com>
2023-12-02 16:34:47 +0700

    fix: remove unused type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-import type { Metadata } from 'next';
-
 import '@/assets/styles/globals.css';
 import inter from '@/assets/fonts/inter';
 import Providers from '@/components/parts/Providers';
 import generateMetadata from '@/lib/metadata';
 
-export const metadata: Metadata = generateMetadata();
+export const metadata = generateMetadata();
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
